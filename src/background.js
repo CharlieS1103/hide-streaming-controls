bc30e008973e3377f41bac7b6529ada81c5f8cb9
CharlieS1103 <someonelikeswindows@gmail.com>
2022-06-26 22:35:54 -0400

    Add peacock support

--- src/background.js
+++ src/background.js
@@ -7,9 +7,7 @@ chrome.runtime.onInstalled.addListener(() => {
 
 // When the user clicks on the extension action
 chrome.action.onClicked.addListener(async (tab) => {
-    console.log(tab.url)
-    console.log(tab.url.startsWith("https://www.hulu.com"))
-    if (tab.url.startsWith("https://www.hulu.com")) {
+    if (tab.url.startsWith("https://www.hulu.com") | tab.url.startsWith("https://www.peacocktv.com")) {
         // We retrieve the action badge to check if the extension is 'ON' or 'OFF'
         const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
         // Next state will always be the opposite
